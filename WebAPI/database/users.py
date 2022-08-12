db60df8769cf334ab9fcdd1e17a3b58e9b7ff9f9
guanlu-up <guanlu.up@gmail.com>
2022-08-12 09:47:34 +0800

    add ArticleDB class; article表的操作

--- WebAPI/database/users.py
+++ WebAPI/database/users.py
@@ -1,9 +1,11 @@
 from database.base import BaseDB
-from models.users import User
+from models.users import User, Article
+
+from models import session
 
 
 class UsersDB(BaseDB):
-    """数据库; Users表"""
+    """数据库; user表"""
 
     def __init__(self):
         super(UsersDB, self).__init__(User)
@@ -16,3 +18,17 @@ class UsersDB(BaseDB):
         """
         query = self._query.filter(self._model.username == username)
         return query.one() if _raise else query.first()
+
+
+class ArticleDB(BaseDB):
+    """数据库; article表"""
+
+    def __init__(self):
+        super(ArticleDB, self).__init__(Article)
+
+    def new_article(self, title: str, content: str, user: User):
+        """创建新文章"""
+        model = self._model(title, content)
+        model.author = user
+        session.add(model)
+        session.commit()
