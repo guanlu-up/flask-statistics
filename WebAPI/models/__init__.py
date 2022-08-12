8d795c0e9267b39e5bad20460d07e3ceb7ca8682
guanlu-up <guanlu.up@gmail.com>
2022-08-12 09:48:54 +0800

    update import...

--- WebAPI/models/__init__.py
+++ WebAPI/models/__init__.py
@@ -1,3 +1,25 @@
+"""
+sqlalchemy.Column():
+    -----args-----
+    :param: name
+    -----kwargs-----
+    :param: autoincrement
+    :param: default
+    :param: doc
+    :param: key
+    :param: index
+    :param: info
+    :param: nullable
+    :param: onupdate
+    :param: primary_key
+    :param: server_default
+    :param: server_onupdate
+    :param: quote
+    :param: unique
+    :param: system
+    :param: comment
+"""
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm.scoping import scoped_session
 
