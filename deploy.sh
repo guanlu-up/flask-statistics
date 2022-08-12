a3c8fa27e5e034e8e023ff7deb0dab2f4bad2114
guanlu-up <guanlu.up@gmail.com>
2022-08-12 16:35:32 +0800

    add docker run container

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-
+# $(pwd) 表示在项目路径下
+# 运行flask镜像服务
 docker run -it --rm \
   -v $(pwd):/root/src/app \
   -p 8001:8001 \
@@ -6,3 +7,13 @@ docker run -it --rm \
   --network app-dev \
   myflaskapp \
   /bin/bash
+
+# 运行mysql镜像服务,将数据映射到本地
+docker run -d \
+  --name mysql-myflask \
+  -p 3306:3306 \
+  -e MYSQL_ROOT_PASSWORD=root \
+  -v $(pwd)/cachedata/mysql/log:/var/log/mysql \
+  -v $(pwd)/cachedata/mysql/data:/var/lib/mysql \
+  -v $(pwd)/cachedata/mysql/conf:/etc/mysql/conf.d \
+  mysql
