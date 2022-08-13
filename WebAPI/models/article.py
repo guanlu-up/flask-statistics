84ac485c206303209abcd17176d7fdea9433a85c
guanlu-up <guanlu.up@gmail.com>
2022-08-13 18:29:31 +0800

    update docstring...

--- WebAPI/models/article.py
+++ WebAPI/models/article.py
@@ -15,7 +15,7 @@ class Article(db.Model):
     # 外键: alchemy.ForeignKey("表名.字段名"); 字段类型要和外键的字段类型保持一致
     author_id = alchemy.Column(alchemy.Integer, alchemy.ForeignKey("user.id"))
 
-    # 多表关系映射; 必须先定义好与对方表进行绑定的外键, 可以实现: articel.author = model
+    # 多表关系映射; 必须先定义好与对方表进行绑定的外键, 可以实现: article.author = model
     # param-1: Model的类名, 必须是已经自定义好的Model。
     # param-2: 反向引用,对方访问我的时候的字段名称
     author = db.relationship("User", backref="articles")
