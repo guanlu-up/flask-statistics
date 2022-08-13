aabb22c529f742561e759302f799b6cb277da8b9
guanlu-up <guanlu.up@gmail.com>
2022-08-13 18:27:53 +0800

    modify users model

--- WebAPI/models/users.py
+++ WebAPI/models/users.py
@@ -13,10 +13,31 @@ class User(db.Model):
     password = alchemy.Column(alchemy.String(256), nullable=False, doc="password")
     is_admin = alchemy.Column(alchemy.Boolean, nullable=False, default=False, doc="is administrator")
 
-    def __init__(self, username: str, password: str, is_admin=False):
+    def __init__(self, username: str, password: str, is_admin=False, extension=None):
         self.username = username
         self.password = password
         self.is_admin = is_admin
+        if extension is None:
+            extension = UserExtension()
+        self.extension = extension
 
     def __repr__(self):
         return "<%s %r>" % (self.__class__, self.username)
+
+
+class UserExtension(db.Model):
+
+    __tablename__ = "user_extension"
+
+    id = alchemy.Column(alchemy.Integer, primary_key=True, autoincrement=True)
+    school = alchemy.Column(alchemy.String(128))
+    interest = alchemy.Column(alchemy.String(128))
+    user_id = alchemy.Column(alchemy.Integer, db.ForeignKey("user.id"))
+
+    # 定义1对1的关系表; db.backref("name") 和直接填 "name"是一样的,但uselist改为False后则变为了1对1
+    # 为True时,对方Model访问我时会返回一个列表; 为False时,对方Model访问我时只会返回一个具体实例
+    user = db.relationship("User", backref=db.backref("extension", uselist=False))
+
+    def __init__(self, school=None, interest=None):
+        self.school = school
+        self.interest = interest
