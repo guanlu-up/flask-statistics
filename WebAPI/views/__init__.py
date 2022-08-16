2f32f49fc258a048d5f25c711ee1943d161ca606
guanlu-up <guanlu.up@gmail.com>
2022-08-16 10:03:21 +0800

    add __init__.py; change package

--- WebAPI/views/__init__.py
+++ WebAPI/views/__init__.py
@@ -3,9 +3,11 @@ from flask import Flask
 from .welcome import welcome_view
 from .users import users_view
 from .article import article_view
+from .auth import auth_view
 
 
 def init_views(app: Flask):
     app.register_blueprint(welcome_view)
     app.register_blueprint(users_view)
     app.register_blueprint(article_view)
+    app.register_blueprint(auth_view)
