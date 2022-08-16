ce1bead68c7e7b4f1f6024db80c0fb71b8c019a6
guanlu-up <guanlu.up@gmail.com>
2022-08-16 10:05:01 +0800

    modify extension.py; add jwt.init_app()

--- WebAPI/extension.py
+++ WebAPI/extension.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
 from .models import db
 
 migrate = Migrate()
+jwt = JWTManager()
 
 
 def extension_register(app: Flask):
     db.init_app(app)
     # db.app = app
     migrate.init_app(app, db)
+    jwt.init_app(app)
