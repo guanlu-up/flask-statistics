bc05fa1f68a120174422341c0281be72684db3d0
guanlu-up <guanlu.up@gmail.com>
2022-08-21 17:51:12 +0800

    change token date to jwt

--- WebAPI/config.py
+++ WebAPI/config.py
@@ -1,6 +1,8 @@
+import datetime
+
 
 class DATABASE(object):
-    HOST = "100.100.21.90"
+    HOST = "192.168.0.114"
     PORT = "3306"
     USERNAME = "root"
     PASSWORD = "root"
@@ -15,3 +17,6 @@ class EnvironConfig(object):
     SQLALCHEMY_DATABASE_URI = "mysql://{}:{}@{}:{}/{}".format(*DATABASE.URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "10#*secret#*%$key!@01"
+
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(hours=1)
+    JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(days=1)
