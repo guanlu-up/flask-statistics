2e02c4f6eaaa178204c78e82fb140ca1c48a9f54
gaoguanlu <gaoguanlu@datagrand.com>
2022-08-23 09:50:26 +0800

    drop invalid method

--- WebAPI/views/welcome.py
+++ WebAPI/views/welcome.py
@@ -11,15 +11,3 @@ def helloworld():
 @welcome_view.route("/welcome", methods=["GET"])
 def welcome_api():
     return redirect(url_for("welcome.helloworld"))
-
-
-@welcome_view.route("/database", methods=["GET"])
-def mysql_testing():
-    from models import db
-    from sqlalchemy.engine.base import Connection, Engine
-    engine: Engine = db.get_engine()
-    connect: Connection = engine.connect()
-    result = connect.execute("select 1")
-    print(result.fetchone())
-    connect.close()
-    return {"status": 200, "message": "database connect success!"}
