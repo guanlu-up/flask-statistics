81eceaf7766748c61c42bd33f113bab39161b957
guanlu-up <guanlu.up@gmail.com>
2022-08-24 15:09:46 +0800

    change framework

--- server.py
+++ server.py
@@ -1,17 +1,4 @@
-from flask import Flask
-
-from WebAPI.views import init_views
-from WebAPI.config import EnvironConfig
-from WebAPI.extension import extension_register
-
-
-def create_app():
-    _app = Flask(__name__)
-    _app.config.from_object(EnvironConfig)
-    init_views(_app)
-    extension_register(_app)
-
-    return _app
+from WebAPI import create_app
 
 
 if __name__ == '__main__':
