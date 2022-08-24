5ec570d8466761cd49840c234bfc6f66bbca5ff2
guanlu-up <guanlu.up@gmail.com>
2022-08-24 15:09:14 +0800

    add redis config and other ENVCONFIG

--- WebAPI/config.py
+++ WebAPI/config.py
@@ -1,5 +1,7 @@
 import datetime
 
+import redis
+
 
 class DATABASE(object):
     HOST = "192.168.0.114"
@@ -11,8 +13,6 @@ class DATABASE(object):
 
 
 class EnvironConfig(object):
-    DEBUG = True
-    ENV = "development" if DEBUG else "production"
     JSON_AS_ASCII = False
     SQLALCHEMY_DATABASE_URI = "mysql://{}:{}@{}:{}/{}".format(*DATABASE.URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -20,3 +20,29 @@ class EnvironConfig(object):
 
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(hours=1)
     JWT_REFRESH_TOKEN_EXPIRES = datetime.timedelta(days=1)
+
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SESSION_USE_SIGNER = True   # 对cookie中的session_id进行混淆处理
+    PERMANENT_SESSION_LIFETIME = (60 * 60) * 24     # session数据的有效期,单位秒
+
+
+class DevelopmentConfig(EnvironConfig):
+    """开发环境的配置"""
+    DEBUG = True
+    ENV = "development"
+
+
+class ProductionConfig(EnvironConfig):
+    """生产环境的配置"""
+    DEBUG = False
+    ENV = "production"
+
+
+ENVIRON_MAPPER = dict(
+    development=DevelopmentConfig,
+    production=ProductionConfig,
+)
