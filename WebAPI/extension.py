dc1047f979dc9c2dc1de7801ff3f802352c0532c
guanlu-up <guanlu.up@gmail.com>
2022-08-24 15:09:28 +0800

    modify docstring...

--- WebAPI/extension.py
+++ WebAPI/extension.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_session import Session
 
 from .models import db
 
+# migrate: 数据库迁移
 migrate = Migrate()
+# jwt: 用于token认证
 jwt = JWTManager()
+# 利用flask-session, 把session保存到redis中
+redis_session = Session()
 
 
 def extension_register(app: Flask):
@@ -13,3 +18,4 @@ def extension_register(app: Flask):
     # db.app = app
     migrate.init_app(app, db)
     jwt.init_app(app)
+    redis_session.init_app(app)
